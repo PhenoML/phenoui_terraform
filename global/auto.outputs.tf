4dc0c227a856c61a3a616e0e1c711cac93ab79f9
Dario Segura <2651846+darionco@users.noreply.github.com>
2024-05-28 13:09:14 -0600

    [GLOBAL] Added placeholder for autogenerated outputs

---
+++ global/auto.outputs.tf
@@ -0,0 +1,6 @@
+output "folder" { value = "[AUTO_GENERATED]" }
+output "project" { value = "[AUTO_GENERATED]" }
+output "region" { value = "[AUTO_GENERATED]" }
+output "bucket" { value = "[AUTO_GENERATED]" }
+output "org_id" { value = "[AUTO_GENERATED]" }
+output "billing_account" { value = "[AUTO_GENERATED]" }
